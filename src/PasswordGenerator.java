77954472596f00cc6f527a5318bc2a65dbecc444
jrheronn <jrhernn@proton.me>
2023-10-09 14:13:12 -0500

    Changed constants to uppercase

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,3 +1,10 @@
 public class PasswordGenerator {
 
+
+    public static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    public static final String NUMBER = "0123456789";
+    public static final String SPECIAL = "!@#$%^&*()-_=+[]{};:',.<>/?";
+    
+
 }
