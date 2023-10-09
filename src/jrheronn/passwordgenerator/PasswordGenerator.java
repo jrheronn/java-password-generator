9afdde5927c9815f6868f139b4304052a95c947b
jrheronn <jrhernn@proton.me>
2023-10-09 14:30:29 -0500

    Created Main class to prompt for length.

--- src/jrheronn/passwordgenerator/PasswordGenerator.java
+++ src/jrheronn/passwordgenerator/PasswordGenerator.java
@@ -1,12 +1,29 @@
 package jrheronn.passwordgenerator;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
-
+    // Declare the final characters that will be used when generate a password.
     public static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
     public static final String NUMBER = "0123456789";
     public static final String SPECIAL = "!@#$%^&*()-_=+[]{};:',.<>/?";
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the password length: ");
+        int length = scanner.nextInt();
+
+        String password = generatePassword(length);
+        System.out.println("Generated Password: " + password);
+
+        scanner.close();
+    }
+
+    private static String generatePassword(int length) {
+    }
+
 
 }
